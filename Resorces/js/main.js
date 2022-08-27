b3aad2bcd1724bd2f1140efe5cb84a43f3b24d52
Md shehab ali <112162874+mdshehabali@users.noreply.github.com>
2022-08-27 22:01:07 +0600

    Add files via upload

---
+++ Resorces/js/main.js
@@ -0,0 +1,11 @@
+// MENU
+
+function openMenu() {
+    document.getElementById('menu-container').style.width = "100%";
+}
+
+function closeMenu() {
+    document.getElementById('menu-container').style.width = "0%";
+}
+// MIX IT UP
+var mixer = mixitup('.work-grid');
